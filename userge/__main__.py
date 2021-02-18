d419158ab37a0d0af0c369687648d900f254ec9c
syahirah-x <79206979+syahirah-x@users.noreply.github.com>
2021-02-18 14:38:25 +0800

    Update __main__.py

--- userge/__main__.py
+++ userge/__main__.py
@@ -9,6 +9,18 @@
 # All rights reserved.
 
 from userge import userge
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+main()
+app = Flask(_name_)
+api = Api(app)
+
+class Greeting (Resource):
+    def get(self):
+        return 'Hello World!'
+api.add_resource(Greeting, '/')
 
 if __name__ == "__main__":
+    app.run('0.0.0.0','8080')
     userge.begin()
